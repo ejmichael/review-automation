5e4dc6345c2e2bd7d2beed59194ebc81ae340197
ejmichael <ethanj.michael03@gmail.com>
2025-06-20 00:55:34 +0200

    added payment functionality to completion

--- client/src/pages/paymentURLs/PaymentSuccess.jsx
+++ client/src/pages/paymentURLs/PaymentSuccess.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 
 const PaymentSuccess = () => {
+      const { user } = useAuthContext()
   return (
     <div className="bg-white py-12">
         <div className="max-w-7xl mx-auto text-center mb-8">
@@ -10,7 +12,7 @@ const PaymentSuccess = () => {
           <p className="mt-4 text-gray-600">Hooray! Your subscription has been processed successfully.</p>
         </div>
 
-      <Link to='/login'>
+      <Link to={`/dashboard/${user.business._id}`}>
         <button className="py-2 px-4 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700">Go to dashboard </button>
       </Link>
     </div>
